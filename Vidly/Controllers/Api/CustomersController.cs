864228d302f95fd8e7c20c208c4d8282cf3665f7
SanicLaptop <chung.andrewchung@gmail.com>
2017-03-18 16:30:35 +0800

    Section 7 Complete: Changed movies/customer index pages to use web API and JSON for datatables to populate fields.

--- Vidly/Controllers/Api/CustomersController.cs
+++ Vidly/Controllers/Api/CustomersController.cs
@@ -8,6 +8,7 @@ using Antlr.Runtime.Tree;
 using AutoMapper;
 using Vidly.Dtos;
 using Vidly.Models;
+using System.Data.Entity;
 
 namespace Vidly.Controllers.Api
 {
@@ -24,7 +25,10 @@ namespace Vidly.Controllers.Api
         // GET /api/customers
         public IHttpActionResult GetCustomers()
         {
-            var customerDtos = _context.Customers.ToList().Select(Mapper.Map<Customer, CustomerDto>);
+            var customerDtos = _context.Customers
+                .Include(c => c.MembershipType)
+                .ToList()
+                .Select(Mapper.Map<Customer, CustomerDto>);
 
             return Ok(customerDtos);
         }
